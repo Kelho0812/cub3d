d8f32fab39ff60b0185a2d239babaff54248c1d8
Jorge Costa <Kelho88@pm.me>
2024-07-30 14:37:15 +0100

    `refactor: Fix memory leak in mapValidator5.c`

--- src/parser/mapValidator5.c
+++ src/parser/mapValidator5.c
@@ -17,6 +17,8 @@ char	**duplicate_map_with_border(char **map, int rows, int cols)
 	char	**new_map;
 
 	new_map = allocate_and_initialize_map(rows, cols);
+	if (new_map == NULL)
+		return (NULL);
 	copy_original_map(new_map, map, rows);
 	return (new_map);
 }
