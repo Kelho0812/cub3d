d6373af8f1fc050e6aa29013366d44fb17c6f625
Jorge Costa <Kelho88@pm.me>
2024-08-08 11:50:32 +0100

    `refactor: Update include path in init.c`

--- src/init.c
+++ src/init.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../includes/cub3d.h"
+#include "../includes/cub3d.h"
 
 void	init_map(t_map *map)
 {
