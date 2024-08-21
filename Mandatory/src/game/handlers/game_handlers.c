c53301d5dddcbfbc9dd777eba4e7ce3ba71fe320
Rui-Pedro-Pires <ruiolive@student.42porto.com>
2024-08-21 16:09:58 +0100

    Add new files and update existing files

--- Mandatory/src/game/handlers/game_handlers.c
+++ Mandatory/src/game/handlers/game_handlers.c
@@ -20,6 +20,7 @@ void	handle_render(t_data *data)
 		data);
 	mlx_hook(data->window.mlx_win, DestroyNotify, NoEventMask, &handle_close,
 		data);
+	mlx_loop_hook(data->window.mlx, render_game, data);
 	mlx_loop(data->window.mlx);
 	mlx_destroy_window(data->window.mlx, data->window.mlx_win);
 	mlx_destroy_display(data->window.mlx);
