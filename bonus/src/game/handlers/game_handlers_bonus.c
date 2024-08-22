f54773ce06bb11f5ac686940c4c4a986463e221a
Jorge Costa <Kelho88@pm.me>
2024-08-22 10:25:20 +0100

    Refactor game_handlers_bonus.c to remove unused code

--- bonus/src/game/handlers/game_handlers_bonus.c
+++ bonus/src/game/handlers/game_handlers_bonus.c
@@ -12,6 +12,7 @@
 
 #include "../../../includes/cub3d_bonus.h"
 
+	// mlx_mouse_hide(data->window.mlx, data->window.mlx_win);
 void	handle_render(t_data *data)
 {
 	mlx_hook(data->window.mlx_win, KeyPress, KeyPressMask, &handle_keypress,
@@ -22,7 +23,6 @@ void	handle_render(t_data *data)
 		data);
 	mlx_hook(data->window.mlx_win, 6, 1L << 6, &handle_mouse, data);
 	mlx_do_key_autorepeaton(data->window.mlx);
-	// mlx_mouse_hide(data->window.mlx, data->window.mlx_win);
 	mlx_loop_hook(data->window.mlx, render_game, data);
 	mlx_loop(data->window.mlx);
 	mlx_destroy_window(data->window.mlx, data->window.mlx_win);
